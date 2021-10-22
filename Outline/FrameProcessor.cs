33c2ae9f06c66bb6e10dc33f275a7cd505aaa184
gregbarnett98 <gregbarnett98@protonmail.com>
2021-10-22 20:17:37 +0100

    Add FrameProccessor

---
+++ Outline/FrameProcessor.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Windows.Graphics.Imaging;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+using WinRT;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Outline
+{
+
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
+    class FrameProcessor
+    {
+        private static Mutex mut = new Mutex();
+        private bool _running = false;
+        private int _framesDropped = 0;
+        private SoftwareBitmap _tmpBuffer;
+        private WriteableBitmap _outputBuffer;
+
+        public FrameProcessor(WriteableBitmap outputBuffer)
+        {
+            _outputBuffer = outputBuffer;
+        }
+
+        public void ProcessFrame(SoftwareBitmap bitmap)
+        {
+            mut.WaitOne();
+            if (_tmpBuffer != null)
+            {
+                _tmpBuffer.Dispose();
+                _tmpBuffer = null;
+                _framesDropped++;
+            }
+            _tmpBuffer = SoftwareBitmap.Copy(bitmap);
+
+            if (!_running)
+            {
+                _running = true;
+                Task.Run(() => doProcessing());
+            }
+
+            mut.ReleaseMutex();
+            bitmap.Dispose();
+
+        }
+
+        private void doProcessing()
+        {
+            mut.WaitOne();
+            if (_tmpBuffer == null)
+            {
+                _running = false;
+                return;
+            }
+            var bitmap = SoftwareBmpToWriteableBmp(_tmpBuffer);
+            _tmpBuffer.Dispose();
+            _tmpBuffer = null;
+            mut.ReleaseMutex();
+
+            DrawFrame(bitmap);
+            _running = false;
+
+        }
+
+        private unsafe WriteableBitmap SoftwareBmpToWriteableBmp(SoftwareBitmap origin)
+        {
+            using (var buffer = origin.LockBuffer(BitmapBufferAccessMode.Read))
+            using (var reference = buffer.CreateReference())
+            {
+                WriteableBitmap output = new WriteableBitmap
+                (
+                    origin.PixelWidth,
+                    origin.PixelHeight,
+                    96,
+                    96,
+                    PixelFormats.Bgra32,
+                    null
+                );
+
+                output.Lock();
+                var out_buffer = output.BackBuffer;
+
+                byte* data;
+                uint capacity;
+                reference.As<IMemoryBufferByteAccess>().GetBuffer(out data, out capacity);
+                int dest_capacity = output.PixelHeight * output.PixelWidth * 4;
+
+                Buffer.MemoryCopy((void*)data, (void*)out_buffer, (long)dest_capacity, (long)(capacity - 1));
+                output.AddDirtyRect(new Int32Rect(0, 0, output.PixelWidth, output.PixelHeight));
+                output.Unlock();
+
+                return output;
+            }
+        }
+
+        private unsafe void DrawFrame(WriteableBitmap frame)
+        {
+            frame.Lock();
+            var frame_buffer = frame.BackBuffer;
+            _outputBuffer.Dispatcher.Invoke(() =>
+            {
+                _outputBuffer.Lock();
+                int capacity = _outputBuffer.PixelWidth * _outputBuffer.PixelHeight * 4;
+                var out_buffer = _outputBuffer.BackBuffer;
+                Buffer.MemoryCopy((void*)frame_buffer, (void*)out_buffer, capacity, capacity);
+                _outputBuffer.AddDirtyRect(new Int32Rect(0, 0, _outputBuffer.PixelWidth, _outputBuffer.PixelHeight));
+                _outputBuffer.Unlock();
+            });
+            frame.Unlock();
+        }
+    }
+
+}
--- Outline/MainWindow.xaml.cs
+++ Outline/MainWindow.xaml.cs
@@ -27,14 +27,6 @@ using WinRT;
 namespace Outline
 {
 
-    [ComImport]
-    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    unsafe interface IMemoryBufferByteAccess
-    {
-        void GetBuffer(out byte* buffer, out uint capacity);
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -46,43 +38,19 @@ namespace Outline
         private SoftwareBitmap _tmpBuffer;
         private WriteableBitmap _bitmap;
         private bool _taskRunning;
+        private FrameProcessor _frameProcessor;
 
         public MainWindow()
         {
-            Trace.WriteLine("Test");
             InitializeComponent();
         }
         
         private async void Window_ContentRendered(object sender, EventArgs e)
         {
-            //await StartFrameReaderAsync();
-            //_mediaCapture = new MediaCapture();
-            //var frame_sources = await MediaFrameSourceGroup.FindAllAsync();
-            //Trace.WriteLine(frame_sources.Count);
-            //Trace.WriteLine(frame_sources[0].DisplayName);
-            //Trace.WriteLine(frame_sources[0].Id);
-            //var infos = frame_sources[0].SourceInfos;
-            //Trace.WriteLine(infos[0].MediaStreamType);
-
-
-            //await _mediaCapture.InitializeAsync(initSettings);
-            //Thread.Sleep(1000);
-            Trace.WriteLine("oi");
-            try
-            {
-                //await _mediaCapture.StartPreviewAsync();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Exception when starting the preview: {0}", ex.ToString());
-            }
-            //Tick();
-        }
-
-        private async void StartClick(object sender, RoutedEventArgs e)
-        {
             await StartFrameReaderAsync();
         }
+
+
 
         private async Task StartFrameReaderAsync()
         {
@@ -90,13 +58,10 @@ namespace Outline
 
             var initSettings = new MediaCaptureInitializationSettings();
             var frame_sources = await MediaFrameSourceGroup.FindAllAsync();
+            // This will crash where no cameras are available
             initSettings.SourceGroup = frame_sources[0];
             initSettings.MemoryPreference = MediaCaptureMemoryPreference.Cpu;
             await _mediaCapture.InitializeAsync(initSettings);
-            //await _mediaCapture.InitializeAsync();
-
-            //var frameInfos = frame_sources[0].SourceInfos[0];
-            //var frameSource = _mediaCapture.FrameSources.FirstOrDefault();
 
             var FrameSource = _mediaCapture.FrameSources.First().Value;
 
@@ -109,22 +74,13 @@ namespace Outline
                 null);
 
             DrawArea.Source = _bitmap;
-            //Trace.WriteLine(FrameSource.Value.SupportedFormats.First().MajorType);
-            //Trace.WriteLine(FrameSource.Value.SupportedFormats.First().Subtype);
-            //Trace.WriteLine(preferredFormat.MajorType);
-            //Trace.WriteLine(preferredFormat.Subtype);
+            _frameProcessor = new FrameProcessor(_bitmap);
 
-            //_frameReader = await _mediaCapture.CreateFrameReaderAsync (FrameSource, MediaEncodingSubtypes.Argb32);
+
             _frameReader = await _mediaCapture.CreateFrameReaderAsync(FrameSource);
             _frameReader.FrameArrived += ProcessFrame;
             await _frameReader.StartAsync();
 
-
-            //await _mediaCapture.StartPreviewAsync();
-            //_timer = new DispatcherTimer();
-            //_timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
-            //_timer.Tick += Tick;
-            //_timer.Start();
         }
 
         private void ProcessFrame(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
@@ -133,54 +89,23 @@ namespace Outline
             var videoMediaFrame = mediaFrameReference?.VideoMediaFrame;
             var frameBitmap = videoMediaFrame?.SoftwareBitmap;
 
-            Trace.WriteLine("ProcessFrame");
-
             if (frameBitmap == null)
             {
-                Trace.WriteLine("Nothing");
                 return;
             }
+
+            var frameBitmapBgra8 = frameBitmap;
             if (frameBitmap is { BitmapPixelFormat: not BitmapPixelFormat.Bgra8 } or { BitmapAlphaMode: not BitmapAlphaMode.Premultiplied })
             {
-               frameBitmap = SoftwareBitmap.Convert(frameBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                frameBitmapBgra8 = SoftwareBitmap.Convert(frameBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                frameBitmap.Dispose();
             }
-            frameBitmap = Interlocked.Exchange(ref _tmpBuffer, frameBitmap);
-            frameBitmap?.Dispose();
-
-            SoftwareBmpToWriteableBmp(_tmpBuffer, _bitmap);
+            _frameProcessor.ProcessFrame(frameBitmapBgra8);
+            frameBitmapBgra8 = Interlocked.Exchange(ref _tmpBuffer, frameBitmapBgra8);
+            frameBitmapBgra8?.Dispose();
 
         }
         
-        private unsafe void SoftwareBmpToWriteableBmp(SoftwareBitmap origin, WriteableBitmap destination)
-        {
-            using (var buffer = origin.LockBuffer(BitmapBufferAccessMode.Read))
-            using (var reference = buffer.CreateReference())
-            {
-                Trace.WriteLine("oi");
-                if (_taskRunning)
-                {
-                    return;
-                }
-                _taskRunning = true;
-
-                _bitmap.Dispatcher.Invoke(() =>
-                {
-                    destination.Lock();
-                    var out_buffer = destination.BackBuffer;
-
-                    byte* data;
-                    uint capacity;
-                    reference.As<IMemoryBufferByteAccess>().GetBuffer(out data, out capacity);
-                    int dest_capacity = destination.PixelHeight * destination.PixelWidth * 4;
-
-                    Buffer.MemoryCopy((void*) data, (void*) out_buffer, (long) dest_capacity, (long) (capacity - 1));
-                    destination.AddDirtyRect(new Int32Rect(0, 0, destination.PixelWidth, destination.PixelHeight));
-
-                    destination.Unlock();
-                });
-                _taskRunning = false;
-            }
-        }
 
 
     }
